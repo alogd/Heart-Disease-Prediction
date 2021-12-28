f7f978d24122ca590827b3f0f90799c988f071d6
teokri303 <teokriwow@gmail.com>
2021-12-28 17:51:10 +0200

    first
    logistic regression added
    created files for other methods

--- linear_regression.py
+++ linear_regression.py
@@ -1,13 +1,26 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
+import seaborn as sns
+
+standardScaler = StandardScaler()
 
 
 row_data = pd.read_csv("Dataset1.csv")
 
-input=row_data.drop("target",1)
-target=row_data["target"]
 
-linear_regression=LinearRegression().fit(input, target)
+
+dataset = pd.get_dummies(row_data, columns = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal'])
+columns_to_scale = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+dataset[columns_to_scale] = standardScaler.fit_transform(dataset[columns_to_scale])
+
+
+
+input = dataset['target']
+target = dataset.drop('target', axis = 1)
+
+linear_regression=LinearRegression().fit(input, [target])
 
 print(linear_regression.score(input, target))
 
