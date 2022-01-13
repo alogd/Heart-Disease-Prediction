5a6438d32645f6a02bc3116947e96557474f15a1
alogd <giwrgos.al.98@gmail.com>
2022-01-13 19:35:41 +0200

    Logistic Regression w/ Variance FS
    Filter Variance Feature Selection Finished

--- logistic_filter.py
+++ logistic_filter.py
@@ -1,8 +1,10 @@
 
-from pandas.core.frame import DataFrame
+from scipy.sparse.construct import random
 from sklearn. feature_selection import mutual_info_classif
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.feature_selection import f_classif
+from statistics import mean
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
@@ -40,44 +42,50 @@ print(X_new.head)
 
 
 
-'''
+
  #---------------------VARIANCE THRESHOLD---------------------------
-
-wanted_columns=['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'target']
+print("\n\nVariance Feature Selection")
 lr=LogisticRegression()
-sel = VarianceThreshold(threshold=0.5)
-# na valoume ton tropo me ton opoio epilegei thn apolutow kauteri timi gia to threshold h test kathe score me
-#diafores times threshold
-
-X,y=dm.get_dataframe(wanted_columns, 'oversampling')
-
-X_train, X_test,y_train, y_test=train_test_split(X,y,test_size=0.3,random_state=40)
-
-print(X_train.shape, X_test.shape)
-print(X.shape)
-
-sel.fit(X_train)
-
-model1=lr.fit(X_train,y_train)
-prediction1=model1.predict(X_test)
-#to score tou monteloy mas prin ginei h afairesh ton features
-print(accuracy_score(y_test,prediction1))
-
-# h metatroph ton feature sta ligotera 
-X_train = sel.transform(X_train)
-X_test = sel.transform(X_test)
-
-print(X_train.shape, X_test.shape)
-
-model1=lr.fit(X_train,y_train)
-prediction1=model1.predict(X_test)
-#to score tou monteloy me ta ligotera features meta thn afairesh me VARIANCE THRESHOLD
-print(accuracy_score(y_test,prediction1))
-'''
 
 
+for sampling in ['oversampling', 'undersampling']:
+    X,y=dm.get_dataframe(sampling=sampling)
+    #We try all the values for threshold between 0 and 1 with a 0.01 step 
+    currentThreshold=0
+    mean_value_per_threshold=[]
 
+    while currentThreshold<=1:
+        sel = VarianceThreshold(threshold=currentThreshold)
+        currentThreshold+=0.01
+        result_of_split=[]
+        #We do 100 different splits and save the mean accuracy value
+        for i in range(100):
+            X_train, X_test,y_train, y_test=train_test_split(X,y,test_size=0.3,random_state=i)
+            sel.fit(X_train)
 
+            #We cut out the under threshold variables
+            X_train = sel.transform(X_train)
+            X_test = sel.transform(X_test)
+            #We train the model 
+            model1=lr.fit(X_train,y_train)
+            prediction1=model1.predict(X_test)
+            #We save the score 
+            result_of_split.append(accuracy_score(y_test,prediction1))
+        #Save result for this threshold value
+        mean_value_per_threshold.append(mean(result_of_split))
+
+    #Plot Threshold and Accuracy Coorelation and print best result 
+    th_values=np.arange(0.0, 1.0, 0.01)
+    max_acc=max(mean_value_per_threshold)
+    max_acc_pos=mean_value_per_threshold.index(max_acc)
+    best_result=f'Max Accuracy: {round(max_acc,3)}  at Threshold: {max_acc_pos/100} with {sampling}' 
+    print(best_result)
+    plt.plot(th_values, mean_value_per_threshold)
+    plt.title(best_result)
+    plt.suptitle('Threshold and Accuracy Coorelation')
+    plt.xlabel('Threshold Value')
+    plt.ylabel('Mean model accuracy for 100 different splits')
+    plt.show()
 
 
 
